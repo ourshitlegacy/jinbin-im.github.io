050e955a32dd1375b25f6b3c20e8d3bd06b0b690
jinbin-im <jinbin.iw@gmail.com>
2025-11-11 15:20:25 +0000

    Deploying to gh-pages from @ jinbin-im/jinbin-im.github.io@b6ff3c160eefe167cd73813de758097da08696a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-conferences",
           title: "conferences",
-          description: "",
+          description: "conferences by categories in reversed chronological order.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/conferences/";
