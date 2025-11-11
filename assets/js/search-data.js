b7c2c7d4d7c324db924a8c1cfbaf2b442ab42e61
jinbin-im <jinbin.iw@gmail.com>
2025-11-11 03:52:57 +0000

    Deploying to gh-pages from @ jinbin-im/jinbin-im.github.io@32f542a81a2e14fecb2821923caa322c6dca14c8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,8 +402,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-spatial-preference-modeling-based-on-immersive-virtual-environment-and-choice-modeling-theory",
+          title: 'Spatial Preference Modeling based on Immersive Virtual Environment and Choice Modeling Theory.',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
