101014ceebef59d811e5c0f5e007ecdf3359f32c
jinbin-im <jinbin.iw@gmail.com>
2025-11-11 06:17:53 +0000

    Deploying to gh-pages from @ jinbin-im/jinbin-im.github.io@838636637b9de65822599b1ac09043f68d7a28f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/conferences/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
