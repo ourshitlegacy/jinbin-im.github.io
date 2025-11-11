797f6f9856ed0b9de5b361488a9a18728bdcf55a
jinbin-im <jinbin.iw@gmail.com>
2025-11-11 03:59:32 +0000

    Deploying to gh-pages from @ jinbin-im/jinbin-im.github.io@795e77d7117dfaea7ac48571ab8217db6ca72616 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,13 +422,13 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
+            },},{id: "projects-study-on-quality-control-of-educational-facilities-using-virtual-environments",
+          title: 'Study on Quality Control of Educational Facilities using Virtual Environments.',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
+            },},{id: "projects-development-of-artificial-intelligence-based-real-time-construction-quality-management-system",
+          title: 'Development of Artificial Intelligence-based Real-Time Construction Quality Management System.',
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
